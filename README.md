0546c1319e6073a0b331593e3de2571fd6d222a3
sharpenedkey <vladislav.malyarchuk@magenta.de>
2022-07-02 18:44:42 +0200

    Created pdf_merger.py with placeholder pdf's for visualizing the process

---
+++ README.md
@@ -0,0 +1,12 @@
+A small Python script that merges all PDF files in a specific directory into one by appending them one after the other.
+
+Usage guide:
+1. Place the script into the same directory as the files to be merged
+2. Make sure the files are numbered in ascending order and named the way you would want the corresponding bookmarks to be named. For example, a file named "2 - CV.pdf" that comes up second in the list would be the second chapter of the resulting PDF file, with a bookmark called "CV" pointing to its beginning.
+3. Run the pdf_merger.py file by double-clicking it
+4. After the script is finished, the resulting file can be found in a sub-directory called "Done"
+
+Notes: 
+- if the original files have their own bookmarks, they will not be transferred to the new file!
+- if some or all files are not numbered, then they will be merged in alphabetic order
+- if the final file "result.pdf" already exists, it gets overwritten after the script is run again
---
+++ pdf_merger.py
@@ -0,0 +1,71 @@
+from PyPDF2 import PdfFileMerger, PdfFileWriter, PdfFileReader
+import os
+
+
+def pdf_merger():
+    
+    """
+    Merges all PDF files in the current directory into one PDF file.
+    """
+
+    # Scans all of the pdf files in the current directory
+    # and creates a list with all of their names
+    path = os.getcwd()
+    pdfs = []
+    for entry in os.scandir(path):
+        if (entry.name.endswith(".pdf")):
+            pdfs.append(entry.name)
+
+    # Merges all of the files from the above list and
+    # also creates a list where the page numbers are listed (i.e.
+    # on which page each new file in the merged pdf will appear)
+    pages = []
+    file_start = 0
+    merger = PdfFileMerger(strict=False)
+    for pdf in pdfs:
+        merger.append(pdf, import_bookmarks=False)
+        cur_file = PdfFileReader(pdf, strict=False)
+        pages.append(file_start)
+        file_start += cur_file.getNumPages()
+        print("Merged file \"" + pdf + "\"")
+    print()
+
+    # Writes the (temporary) merged pdf file in a folder called
+    # "Done", which is created if it doesn't exist
+    if not os.path.isdir(os.getcwd() + "/Done"):
+        os.makedirs(os.getcwd() + "/Done")
+    merger.write("Done/result.pdf")
+
+    # Initializes the file writer and the file reader
+    # for the final pdf file
+    writer = PdfFileWriter()
+    reader = PdfFileReader("Done/result.pdf")
+
+    # Re-creates Bewerbung.pdf using the file writer in order
+    # to then create actually working bookmarks (apparently
+    # FileMerger can't do it well enough)
+    input_numpages = reader.getNumPages()
+    for i in range(input_numpages):
+        writer.addPage(reader.getPage(i))
+
+    # Creates the bookmarks named just like the original pdf
+    # files' names
+    for pdf in pdfs:
+        name_without_number = str.strip(pdf, "1234567890 -")
+        name_without_pdf = str.rstrip(name_without_number[:-4])
+        writer.addBookmark(name_without_pdf, pages[pdfs.index(pdf)], parent=None)
+        print("Created bookmark \"" + name_without_pdf + "\"")
+    print()
+    writer.setPageMode("/UseOutlines")
+
+    # Writes the newly re-created (now final) Bewerbung.pdf file
+    with open("Done/result.pdf", "wb") as fp:
+        writer.write(fp)
+
+    # Prints the final output
+    print("Finished!")
+    print("The final file is located at: " + os.getcwd() + "\\Done\\result.pdf\n")
+    input("Press ENTER to exit")
+
+
+pdf_merger()
